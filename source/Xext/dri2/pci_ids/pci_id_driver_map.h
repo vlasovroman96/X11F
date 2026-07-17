b63c6838d0400224eef29edeef3362bbba6497fe
vlasovroman96 <vlasovroman.ru@yandex.ru>
2026-07-17 17:00:06 +0700

    Semantic 3: 60 files passes

--- source/Xext/dri2/pci_ids/pci_id_driver_map.h
+++ source/Xext/dri2/pci_ids/pci_id_driver_map.h
@@ -4,7 +4,7 @@
 #include <stddef.h>
 
 #ifndef ARRAY_SIZE
-#define ARRAY_SIZE(a) (sizeof(a) / sizeof((a)[0]))
+#define mixin(ARRAY_SIZE!("a")) (sizeof(a) / sizeof((a)[0]))
 #endif
 
 static const int i915_chip_ids[] = {
@@ -47,19 +47,19 @@ static const struct {
    const int *chip_ids;
    int num_chips_ids;
 } driver_map[] = {
-   { 0x8086, "i915", i915_chip_ids, ARRAY_SIZE(i915_chip_ids) },
-   { 0x8086, "i965", i965_chip_ids, ARRAY_SIZE(i965_chip_ids) },
+   { 0x8086, "i915", i915_chip_ids, mixin(ARRAY_SIZE!("i915_chip_ids")) },
+   { 0x8086, "i965", i965_chip_ids, mixin(ARRAY_SIZE!("i965_chip_ids")) },
    { 0x8086, "i965", NULL, -1 },
 #ifndef DRIVER_MAP_GALLIUM_ONLY
-   { 0x1002, "radeon", r100_chip_ids, ARRAY_SIZE(r100_chip_ids) },
-   { 0x1002, "r200", r200_chip_ids, ARRAY_SIZE(r200_chip_ids) },
+   { 0x1002, "radeon", r100_chip_ids, mixin(ARRAY_SIZE!("r100_chip_ids")) },
+   { 0x1002, "r200", r200_chip_ids, mixin(ARRAY_SIZE!("r200_chip_ids")) },
 #endif
-   { 0x1002, "r300", r300_chip_ids, ARRAY_SIZE(r300_chip_ids) },
-   { 0x1002, "r600", r600_chip_ids, ARRAY_SIZE(r600_chip_ids) },
+   { 0x1002, "r300", r300_chip_ids, mixin(ARRAY_SIZE!("r300_chip_ids")) },
+   { 0x1002, "r600", r600_chip_ids, mixin(ARRAY_SIZE!("r600_chip_ids")) },
    { 0x1002, "radeonsi", NULL, -1 },
    { 0x10de, "nouveau", NULL, -1 },
-   { 0x1af4, "virtio_gpu", virtio_gpu_chip_ids, ARRAY_SIZE(virtio_gpu_chip_ids) },
-   { 0x15ad, "vmwgfx", vmwgfx_chip_ids, ARRAY_SIZE(vmwgfx_chip_ids) },
+   { 0x1af4, "virtio_gpu", virtio_gpu_chip_ids, mixin(ARRAY_SIZE!("virtio_gpu_chip_ids")) },
+   { 0x15ad, "vmwgfx", vmwgfx_chip_ids, mixin(ARRAY_SIZE!("vmwgfx_chip_ids")) },
    { 0x0000, NULL, NULL, 0 },
 };
 
