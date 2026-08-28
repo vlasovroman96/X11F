c207534d7d15966be69ad06a0acf169794f8dcf9
Roman Vlasov <vlasovroman.ru@yandex.ru>
2026-08-28 14:14:51 +0700

    Fixed COMPOSITE extension compilation conds

--- source/build/dix_config.d
+++ source/build/dix_config.d
@@ -24,7 +24,7 @@ enum BUILD_GLX_DRI = 1;
 
 enum COMPILEDDEFAULTFONTPATH = "/usr/share/fonts/misc,/usr/share/fonts/TTF,/usr/share/fonts/OTF,/usr/share/fonts/Type1,/usr/share/fonts/100dpi,/usr/share/fonts/75dpi";
 
-enum COMPOSITE = 1;
+version = COMPOSITE;
 
 enum CONFIG_MITSHM = 1;
 
